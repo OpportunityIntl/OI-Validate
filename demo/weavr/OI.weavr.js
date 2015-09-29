a16a59c00aa95cca2e53325b6300a325ed7593bd
Joe Weber <wosephjeber@gmail.com>
2015-09-28 21:39:37 -0500

    Initial gh-pages commit

---
+++ demo/weavr/OI.weavr.js
@@ -0,0 +1,231 @@
+/* Modernizr 2.8.3 (Custom Build) | MIT & BSD
+ * Build: http://modernizr.com/download/#-backgroundsize-flexbox-opacity-rgba-csstransforms-video-inlinesvg-svg-teststyles-testprop-testallprops-prefixes-domprefixes-css_calc-css_vhunit-css_vmaxunit-css_vminunit-css_vwunit
+ */
+;window.Modernizr=function(a,b,c){function z(a){i.cssText=a}function A(a,b){return z(l.join(a+";")+(b||""))}function B(a,b){return typeof a===b}function C(a,b){return!!~(""+a).indexOf(b)}function D(a,b){for(var d in a){var e=a[d];if(!C(e,"-")&&i[e]!==c)return b=="pfx"?e:!0}return!1}function E(a,b,d){for(var e in a){var f=b[a[e]];if(f!==c)return d===!1?a[e]:B(f,"function")?f.bind(d||b):f}return!1}function F(a,b,c){var d=a.charAt(0).toUpperCase()+a.slice(1),e=(a+" "+n.join(d+" ")+d).split(" ");return B(b,"string")||B(b,"undefined")?D(e,b):(e=(a+" "+o.join(d+" ")+d).split(" "),E(e,b,c))}var d="2.8.3",e={},f=b.documentElement,g="modernizr",h=b.createElement(g),i=h.style,j,k={}.toString,l=" -webkit- -moz- -o- -ms- ".split(" "),m="Webkit Moz O ms",n=m.split(" "),o=m.toLowerCase().split(" "),p={svg:"http://www.w3.org/2000/svg"},q={},r={},s={},t=[],u=t.slice,v,w=function(a,c,d,e){var h,i,j,k,l=b.createElement("div"),m=b.body,n=m||b.createElement("body");if(parseInt(d,10))while(d--)j=b.createElement("div"),j.id=e?e[d]:g+(d+1),l.appendChild(j);return h=["&#173;",'<style id="s',g,'">',a,"</style>"].join(""),l.id=g,(m?l:n).innerHTML+=h,n.appendChild(l),m||(n.style.background="",n.style.overflow="hidden",k=f.style.overflow,f.style.overflow="hidden",f.appendChild(n)),i=c(l,a),m?l.parentNode.removeChild(l):(n.parentNode.removeChild(n),f.style.overflow=k),!!i},x={}.hasOwnProperty,y;!B(x,"undefined")&&!B(x.call,"undefined")?y=function(a,b){return x.call(a,b)}:y=function(a,b){return b in a&&B(a.constructor.prototype[b],"undefined")},Function.prototype.bind||(Function.prototype.bind=function(b){var c=this;if(typeof c!="function")throw new TypeError;var d=u.call(arguments,1),e=function(){if(this instanceof e){var a=function(){};a.prototype=c.prototype;var f=new a,g=c.apply(f,d.concat(u.call(arguments)));return Object(g)===g?g:f}return c.apply(b,d.concat(u.call(arguments)))};return e}),q.flexbox=function(){return F("flexWrap")},q.rgba=function(){return z("background-color:rgba(150,255,150,.5)"),C(i.backgroundColor,"rgba")},q.backgroundsize=function(){return F("backgroundSize")},q.opacity=function(){return A("opacity:.55"),/^0.55$/.test(i.opacity)},q.csstransforms=function(){return!!F("transform")},q.video=function(){var a=b.createElement("video"),c=!1;try{if(c=!!a.canPlayType)c=new Boolean(c),c.ogg=a.canPlayType('video/ogg; codecs="theora"').replace(/^no$/,""),c.h264=a.canPlayType('video/mp4; codecs="avc1.42E01E"').replace(/^no$/,""),c.webm=a.canPlayType('video/webm; codecs="vp8, vorbis"').replace(/^no$/,"")}catch(d){}return c},q.svg=function(){return!!b.createElementNS&&!!b.createElementNS(p.svg,"svg").createSVGRect},q.inlinesvg=function(){var a=b.createElement("div");return a.innerHTML="<svg/>",(a.firstChild&&a.firstChild.namespaceURI)==p.svg};for(var G in q)y(q,G)&&(v=G.toLowerCase(),e[v]=q[G](),t.push((e[v]?"":"no-")+v));return e.addTest=function(a,b){if(typeof a=="object")for(var d in a)y(a,d)&&e.addTest(d,a[d]);else{a=a.toLowerCase();if(e[a]!==c)return e;b=typeof b=="function"?b():b,typeof enableClasses!="undefined"&&enableClasses&&(f.className+=" "+(b?"":"no-")+a),e[a]=b}return e},z(""),h=j=null,e._version=d,e._prefixes=l,e._domPrefixes=o,e._cssomPrefixes=n,e.testProp=function(a){return D([a])},e.testAllProps=F,e.testStyles=w,e}(this,this.document),Modernizr.addTest("csscalc",function(){var a="width:",b="calc(10px);",c=document.createElement("div");return c.style.cssText=a+Modernizr._prefixes.join(b+a),!!c.style.length}),Modernizr.addTest("cssvhunit",function(){var a;return Modernizr.testStyles("#modernizr { height: 50vh; }",function(b,c){var d=parseInt(window.innerHeight/2,10),e=parseInt((window.getComputedStyle?getComputedStyle(b,null):b.currentStyle).height,10);a=e==d}),a}),Modernizr.addTest("cssvmaxunit",function(){var a;return Modernizr.testStyles("#modernizr { width: 50vmax; }",function(b,c){var d=window.innerWidth/100,e=window.innerHeight/100,f=parseInt((window.getComputedStyle?getComputedStyle(b,null):b.currentStyle).width,10);a=parseInt(Math.max(d,e)*50,10)==f}),a}),Modernizr.addTest("cssvminunit",function(){var a;return Modernizr.testStyles("#modernizr { width: 50vmin; }",function(b,c){var d=window.innerWidth/100,e=window.innerHeight/100,f=parseInt((window.getComputedStyle?getComputedStyle(b,null):b.currentStyle).width,10);a=parseInt(Math.min(d,e)*50,10)==f}),a}),Modernizr.addTest("cssvwunit",function(){var a;return Modernizr.testStyles("#modernizr { width: 50vw; }",function(b,c){var d=parseInt(window.innerWidth/2,10),e=parseInt((window.getComputedStyle?getComputedStyle(b,null):b.currentStyle).width,10);a=e==d}),a});
+;var Tooltip = function(elem, opts) {
+  // cache this
+  var _this = this;
+  
+  // make elem a public property
+  this.elem = elem;
+  
+  // default options
+  var options = {
+    position: _this.elem.data('tooltip-position') || 'top',
+    message: _this.elem.data('tooltip'),
+    max: _this.elem.data('tooltip-width') || 300,
+    style: _this.elem.data('tooltip-style') || 'purple',
+    zindex: _this.elem.data('tooltip-zindex') || null
+  };
+  
+  // extend default options with 'opts' argument
+  $.extend(options, opts);
+  
+  // create jQuery object of tooltip
+  this.tip = $('<div class="tooltip ' + options.position + '"><div class="tooltip-container ' + options.style + '"><div class="tooltip-content">' + options.message + '</div></div></div>');
+  
+  if (options.zindex) {
+    _this.tip.css('z-index', options.zindex);
+  }
+  
+  // append tooltip to body
+  $('body').append(this.tip);
+  
+  var tipContent = _this.tip.find('.tooltip-content');
+  var tipContainer = _this.tip.find('.tooltip-container');
+  
+  // method to position the tooltip
+  this.setPosition = function() {
+    var elemOffsetX = elem.offset().left;
+    var elemOffsetY = elem.offset().top;
+    var elemWidth = elem.outerWidth();
+    var elemHeight = elem.outerHeight();
+    
+    _this.tip.css({
+      top: elem.offset().top + (options.position === 'bottom' ? elemHeight : 0) + 'px',
+      left: Math.round((elem.offset().left + (elemWidth / 2))) + 'px',
+      position: 'absolute'
+    });
+    
+    // return _this to allow for chaining
+    return _this;
+  };
+  
+  // method to set the width of the tooltip
+  this.setWidth = function() {
+    // make tooltip 'visible' to browser so we can get the content width
+    _this.tip.css({'display': 'block', 'visibility': 'hidden'});
+    
+    // reset tooltip width so we can get the natural width of the content
+    tipContainer.css('width', '');
+    var naturalWidth = tipContent.outerWidth();
+    
+    // check if options.max is a percentage or integer
+    // if it's a percentage, set the max width to that percentage of the element
+    var maxWidth = (typeof options.max === 'string' && options.max.slice(-1) === '%') ? (_this.elem.outerWidth() * (options.max.slice(0, -1) / 100)) : options.max;
+    if (naturalWidth > maxWidth) {
+      tipContainer.css('width', maxWidth);
+    } else {
+      tipContainer.css('width', naturalWidth);
+    }
+    
+    // hide the tooltip again
+    _this.tip.css({'display': 'none', 'visibility': 'visible'});
+    
+    // return _this to allow for chaining
+    return _this;
+  };
+  
+  // method to show the tooltip
+  this.show = function() {
+    _this.setPosition().setWidth();
+    _this.tip.stop().fadeIn(500);
+    
+    // return _this to allow for chaining
+    return _this;
+  };
+  
+  // method to hide the tooltip
+  this.hide = function() {
+    _this.tip.stop().fadeOut(500);
+    
+    // return _this to allow for chaining
+    return _this;
+  };
+  
+  // attach event handlers when the tooltip object is instantiated
+  this.elem.hover(function() {
+    _this.show();
+  }, function() {
+    _this.hide();
+  });
+  this.tip.hover(function() {
+    _this.show();
+  }, function() {
+    _this.hide();
+  });
+  
+};
+;var OI = OI || {};
+
+OI.utils = {
+  
+  popover : function (elem, pos, msg) {
+    var pop = $('<div class="popover ' + pos + '"><div>' + msg + '</div></div>');
+    if (pos == 'top') {
+      elem.before(pop);
+    } else if (pos == 'bottom') {
+      elem.after(pop);
+    }
+    
+    pop.children().delay(100).fadeIn(500).delay(4000).fadeOut(500,function(){
+      pop.remove();
+    });
+    
+    elem.focusin(function(){
+      pop.remove();
+    });
+  },
+  
+  limitToRegex : function(input,regex) {
+    input.keypress(function(e){
+      var k = e.which;
+      var keychar = String.fromCharCode(k);
+      if ([0,8,13,32].indexOf(k) != -1 || regex.test(keychar)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  },
+  
+  setupSelect : function(elem) {
+    elem.focus(function(){
+      elem.parents('.select').addClass('focus');
+    }).blur(function(){
+      elem.parents('.select').removeClass('focus');
+    });
+    elem.change(function(){
+      if (elem.val() === '') {
+        elem.addClass('select-placeholder');
+      } else {
+        elem.removeClass('select-placeholder');
+      }
+    });
+    
+    // Add placeholder class on init if value is blank
+    if (elem.val() === '') {
+      elem.addClass('select-placeholder');
+    }
+  },
+  
+  validateEmail : function(email) {
+		var re = /^[0-9a-zA-Z][-.+_a-zA-Z0-9]*@([0-9a-zA-Z][-._0-9a-zA-Z]*\.)+[a-zA-Z]{2,6}$/;
+		return re.test(email);
+	}
+};
+
+// set up select input placeholder styling
+$('.select select').each(function(){
+	OI.utils.setupSelect($(this));
+});
+
+// set up tooltips
+$('[data-tooltip]').each(function() {
+  new Tooltip($(this));
+});
+
+// disable links styled as buttons with .disabled class
+$('body').on('click.disabledButton', 'a.btn.disabled', function() {
+  return false;
+});
+
+// set up character restrictions on inputs (does not work on Android)
+$('[data-pattern]').each(function(){
+	var pattern = $(this).data('pattern');
+	var regex;
+	if (pattern == 'numeric') {
+		regex = /[0-9]/;
+	} else if (pattern == 'alpha') {
+		regex = /[a-zA-Z]/;
+	} else if (pattern == 'alphanumeric') {
+		regex = /[a-zA-Z0-9]/;
+	} else {
+		regex = new RegExp(pattern);
+	}
+	OI.utils.limitToRegex($(this),regex);
+});
+;var OI = OI || {};
+
+OI.layout = {
+  init: function() {
+    OI.layout.overlayHeightFix();
+    
+    if (!Modernizr.csstransforms) {
+      OI.layout.verticalAlignCenterPolyfill();
+    }
+    
+    $(window).resize(function() {
+      OI.layout.overlayHeightFix();
+    });
+  },
+  
+  overlayHeightFix: function() {
+    $('.content-overlay').each(function() {
+      var contentHeight = $(this).outerHeight(true);
+      var containerHeight = $(this).parent().height();
+      if (contentHeight > containerHeight) {
+        $(this).parent().css('min-height', contentHeight);
+      }
+    });
+  },
+  
+  verticalAlignCenterPolyfill: function() {
+    $('.content-overlay.middle').each(function() {
+      var contentHeight = $(this).outerHeight(true);
+      $(this).css('margin-top', -contentHeight / 2);
+    });
+  }
+};
+
+OI.layout.init();
---
+++ js/OI.validate.js
@@ -0,0 +1,466 @@
+(function($) {
+  $.fn.validate = function(options) {
+    return this.each(function() {
+      new Validator($(this), options);
+    });
+  };
+}(jQuery));
+
+var Validator = function(form, options) {
+  var _this = this;
+  
+  this.options = $.extend({
+    onInit: function() {},
+    onSubmit: function() {},
+    onError: function() {},
+    onSuccess: function() {},
+    showProcessing: showProcessing,
+    hideProcessing: hideProcessing,
+    formValidations: [],
+    fieldValidations: [],
+    showAlert: showAlert,
+    hideAlert: hideAlert
+  }, options);
+  
+  this.validations = [
+    {
+      field: 'input[type!="radio"][required], select[required], textarea[required]',
+      validation: function(field) {
+        return field.val() !== '';
+      },
+      fieldMessage: 'Required field',
+      formMessage: 'Please fill out missing fields'
+    },
+    {
+      field: 'input[type="email"]',
+      validation: function(field) {
+        var re = /^[0-9a-zA-Z][-.+_a-zA-Z0-9]*@([0-9a-zA-Z][-._0-9a-zA-Z]*\.)+[a-zA-Z]{2,6}$/;
+        return re.test(field.val());
+      },
+      fieldMessage: 'Invalid email address',
+      formMessage: 'Please enter valid email address'
+    },
+    {
+      field: 'input[type="radio"][required]',
+      validation: function(field) {
+        var name = field.attr('name');
+        return this.find('input[type="radio"][name="' + name + '"]:checked').length > 0;
+      },
+      fieldMessage: 'Select an option',
+      formMessage: 'Please fill out missing fields'
+    }
+  ];
+  
+  /**** Public attributes ****/
+  
+  this.form = $(form);
+  this.fields = $();
+  this.errorMessages = [];
+  
+  /**** Public methods ****/
+  
+  // validate the whole form
+  this.validate = function() {
+    // start error counter at 0
+    var errors = 0;
+    
+    // validate each field and increase error counter if validation fails
+    _this.fields.each(function() {
+      if (!_this.validateField($(this))) errors++;
+    });
+    
+    // run custom form validations
+    $.each(_this.options.formValidations, function(index, item) {
+      if (!item.validation.call(_this, _this.form)) {
+        if (item.formMessage) _this.errorMessages.push(item.formMessage);
+        errors++;
+      }
+    });
+    
+    // return true or false
+    return (errors > 0) ? false : true;
+  };
+  
+  // validate a single field
+  this.validateField = function(field) {
+    // get plugin data for this field
+    var data = getData(field);
+    
+    // start by assuming field is valid
+    var validity = true;
+    
+    // if field has data, run through its validations
+    if (data) {
+      $.each(data.validations, function(index, item) {
+        if (!item.validation.call(_this.form, field)) {
+          // field failed this validation
+          
+          // display field error  
+          if (item.fieldMessage) {
+            
+            if (data.errorMessages.indexOf(item.fieldMessage) === -1) {
+              data.errorMessages.push(item.fieldMessage);
+              setData(field, data);
+              
+              _this.displayError(field, item.fieldMessage);
+            }
+          }
+          
+          // add message for alert
+          if (item.formMessage) _this.errorMessages.push(item.formMessage);
+          
+          // set validity to false (we'll return this value at the end)
+          validity = false;
+        } else {
+          // field passed this validation
+          
+          // remove error from field data
+          var errorIndex = data.errorMessages.indexOf(item.fieldMessage);
+          if (errorIndex !== -1) {
+            data.errorMessages.splice(data.errorMessages.indexOf(item.fieldMessage), 1);
+            setData(field, data);
+            
+            // hide field error
+            _this.hideError(field, item.fieldMessage);
+          }
+          
+          // set validity to true
+          validity = true;
+        }
+      });
+    }
+    
+    return validity;
+  };
+  
+  // display error message on field
+  this.displayError = function(field, message) {
+    // get plugin data for this field
+    var data = getData(field);
+    
+    // create new element for error message
+    var errorMessage = $('<div>', {class: 'error-message', style: 'display: none'});
+    
+    // set content for error message
+    errorMessage.html(message);
+    
+    // insert error message after field
+    data.element.after(errorMessage);
+    
+    // show error message
+    errorMessage.slideDown(500);
+    
+    // add error class to field
+    data.element.addClass('error');
+    
+    return _this;
+  };
+  
+  // hide error message on field
+  this.hideError = function(field, error) {
+    var data = getData($(field));
+    
+    // get error message object (it's a sibling of the field)
+    var errorIndex = data.errorMessages.indexOf(error);
+    var errorMessage = data.element.siblings('.error-message:contains("' + error + '")');
+    
+    // remove error message
+    errorMessage.slideUp(500, function() {
+      errorMessage.remove();
+    });
+    
+    // remove error class from field
+    data.element.removeClass('error');
+    
+    return _this;
+  };
+  
+  // display error message for whole form
+  this.displayErrors = function() {
+    // filter error messages to remove duplicates
+    var uniqueMessages = _this.errorMessages.reduce(function(previousValue, currentValue){
+      if (previousValue.indexOf(currentValue) < 0 ) previousValue.push(currentValue);
+      return previousValue;
+    },[]);
+    
+    // generate alert message string
+    var message = "<p><strong>Looks like there are some problems with the highlighted fields. Please address the following errors:</strong></p>";
+    message += "<ul>";
+    $.each(uniqueMessages, function(index, errorMessage) {
+      message += "<li>" + errorMessage + "</li>";
+    });
+    message += "</ul>";
+    
+    // show alert
+    _this.alert(message);
+    
+    return _this;
+  };
+  
+  // show alert
+  this.alert = function(message, classes) {    
+    // run callback function
+    _this.options.showAlert.call(_this.form, message, classes);
+    
+    return _this;
+  };
+  
+  this.hideAlert = function() {
+    _this.options.hideAlert.call(_this.form);
+    
+    return _this;
+  };
+  
+  // hide error message for whole form
+  this.hideErrors = function() {
+    _this.hideAlert();
+    
+    // empty error messages array
+    _this.errorMessages = [];
+    
+    // hide field errors
+    _this.fields.each(function(index, field) {
+      var data = getData(field);
+      
+      $.each(data.errorMessages, function(index, error) {
+        _this.hideError($(field), error);
+        data.errorMessages.splice(index, 1);
+        
+        setData($(field), data);
+      });
+    });
+    
+    return _this;
+  };
+  
+  // enable/disable instant field validation
+  this.validateOnBlur = function(boolean) {
+    if (boolean) {
+      _this.fields.on('blur.validate change.validate', function() {
+        _this.validateField($(this));
+      });
+    } else {
+      _this.fields.off('blur.validate change.validate');
+    }
+    
+    return _this;
+  };
+  
+  // add a single field to the list of fields to be validated
+  this.addField = function(field) {
+    // add field
+    _this.fields = _this.fields.add(field);
+    
+    return _this;
+  };
+  
+  this.processing = function(boolean) {
+    if (boolean) {
+      _this.options.showProcessing.call(_this, _this.form);
+    } else {
+      _this.options.hideProcessing.call(_this, _this.form);
+    }
+    
+    return _this;
+  };
+  
+  /**** Private methods ****/
+  
+  // special validation for radio buttons
+  function validateRadio(field) {
+    
+  }
+  
+  // special validation for checkboxes
+  function validateCheckbox(field) {
+    
+  }
+  
+  // get plugin data for a field
+  function getData(field) {
+    // get data stored by jQuery in 'oi-validate'
+    var data = $(field).data('oi-validate');
+    
+    // if there's any data, return it
+    // otherwise return false
+    if (data) {
+      return data;
+    } else {
+      return false;
+    }
+  }
+  
+  // set plugin data for a field
+  function setData(field, newData) {
+    // get existing data for field, if any
+    var existingData = getData($(field)) || {};
+    
+    // extend existing data with new data
+    data = $.extend(existingData, newData);
+    
+    // save data to field via jQuery
+    $(field).data('oi-validate', data);
+  }
+  
+  function setupValidation(field, validation) {
+    var data = getData(field) || {
+      validations: [],
+      element: $(field),
+      errorMessages: []
+    };
+    
+    data.validations.push(validation);
+    
+    // if field is a select input, set "element" to its parent div
+    // (this is specific to how select inputs are styled in Weavr)
+    if ($(field).is('select')) {
+      data.element = $(field).parents('.select');
+    }
+    
+    // if field is a radio input, we want to display the error message after
+    // the last label in the radio group
+    if ($(field).is('input[type="radio"]')) {
+      data.element = $(field).siblings('[name="' + $(field).attr('name') + '"]').last().siblings('label');
+    }
+    
+    // set this data for the field
+    setData($(field), data);
+  }
+  
+  // setup validations for a field
+  function setupValidations() {
+    // extend default validations with custom field validations
+    $.each(_this.options.fieldValidations, function(index, validation) {
+      _this.validations.push(validation);
+    });
+    
+    // set up fields and validation data for each validation
+    $.each(_this.validations, function(index, validation) {
+      $(_this.form.find(validation.field)).each(function(index, field) {
+        
+        // if field is a radio input... 
+        if ($(field).is('input[type="radio"]')) {
+          // check to see if another radio input in the same group has already
+          // been added to the list of fields to avoid duplicate error messages
+          if (_this.fields.filter('input[type="radio"][name="' + $(field).attr('name') + '"]').length > 0) {
+            return;
+          } else {
+            // if it's the first radio input of a group, set the field object to
+            // reference the radio group instead of the individual radio input
+            field = _this.form.find('input[type="radio"][name="' + $(field).attr('name') + '"]');
+          }
+        }
+        
+        _this.addField($(field));
+        setupValidation($(field), validation);
+      });
+    });
+  }
+  
+  // add validation to a field
+  function addValidation(field, validation) {
+    var data = getData(field) || {};
+    data.validations.push(validation);
+    setData(field, data);
+  }
+  
+  // default callback to show alert
+  function showAlert(message, classes) {
+    // create alert div, or empty it if it already exists
+    var alert = $(this).find('.alert');
+    
+    // if alert already exists, remove it
+    if (alert.length > 0) {
+      alert.remove();
+    }
+    
+    // create alert element
+    alert = $('<div>', {class: 'alert ' + (classes ? classes : 'error'), style: "display: none"});
+    
+    // prepend alert to form
+    $(this).prepend(alert);
+    
+    // set alert content
+    alert.html(message);
+    
+    alert.slideDown(500);
+  }
+  
+  // default callback to hide alert
+  function hideAlert() {
+    var alert = $(this).find('.alert');
+    alert.slideUp(500, function() {
+      alert.remove();
+    });
+  }
+  
+  // default callback to show processing indicator
+  function showProcessing(form) {
+    var submitButton = form.find('input[type="submit"]');
+    var processingButton = $('<button>', {type: 'button', id: 'processing-btn', class: submitButton.attr('class'), disabled: true});
+    processingButton.html('Processing...');
+    submitButton.hide();
+    submitButton.after(processingButton);
+  }
+  
+  // default callback to hide processing indicator
+  function hideProcessing(form) {
+    var submitButton = form.find('input[type="submit"]');
+    submitButton.show();
+    $('#processing-btn').hide();
+  }
+  
+  // set up submit handler and other misc things on form
+  function setupForm() {
+    // add 'novalidate' attribute to form to prevent native browser error handling
+    _this.form.attr('novalidate', 'novalidate');
+    
+    _this.hideErrors();
+    
+    // add event handler to validate form on submit
+    _this.form.submit(function() {
+      // execute onSubmit callback and stop execution if callback returns false
+      if (_this.options.onSubmit.call(_this, _this.form) === false) {
+        return false;
+      }
+      
+      _this.hideErrors();
+      
+      // remove field validation on blur (to prevent multiple handlers building up)
+      _this.validateOnBlur(false);
+      
+      if (_this.validate()) {
+        // form passed validation
+        
+        // execute success callback
+        if (typeof _this.options.onSuccess === 'function') {
+          if (_this.options.onSuccess.call(_this, _this.form) === false) {
+            return false;
+          }
+        }
+        
+      } else {
+        // form failed validation
+        
+        // display form error message
+        _this.displayErrors();
+        
+        // set up field validation on blur
+        _this.validateOnBlur(true);
+        
+        // execute error callback
+        if (typeof _this.options.onError === 'function') _this.options.onError.call(_this, _this.form);
+        
+        // return false to prevent form from submitting so we can run validations
+        return false;
+        
+      }
+    });
+    
+    setupValidations();
+    
+    _this.options.onInit.call(_this, _this.form);
+  }
+  
+  setupForm();
+};
